b53bf7ed7828a3e5fa754d9b860f6792aa52915b
huzaifa1230 <huzaifairshad173@gmail.com>
2024-12-21 21:49:33 +0500

    feat: project setup + tailwind integration

---
+++ crypto-web/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-blue-600">
+      <h1 className="text-purple-700">hello from crypto</h1>
+    </div>
+  );
+}
+
+export default App;
